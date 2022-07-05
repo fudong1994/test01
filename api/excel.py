78e3bbce08cb09b585a105fdb7291be944cc6c86
wufudong <wfd19940918@163.com>
2022-07-05 13:08:35 +0800

    提交代码

--- api/excel.py
+++ api/excel.py
@@ -4,9 +4,11 @@ workbook = openpyxl.load_workbook("常规二类台账_补数台账20220705.xlsx"
 
 sh = workbook["常规二类授信"]
 res = list(sh.columns)
-
-with open('sql001_test', mode='w', encoding='utf-8') as f:
+count = 0
+with open('sql01_test.txt', mode='w', encoding='utf-8') as f:
     for i in res[1][1:]:
         sql = "update hlpj_project set regular_class = 1 where project_id = {};".format(i.value)
         f.write(sql)
         f.write("\n")
+        count += 1
+    print("共处理了{}条数据".format(count))
